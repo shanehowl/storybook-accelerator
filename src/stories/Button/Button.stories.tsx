093d80dd861c223820fc47eb0dff9c11f588f757
Shane <shanehowl@hotmail.com>
2021-03-14 22:35:50 +0800

    Added storybook to figma plugin and added sample to button story

--- src/stories/Button/Button.stories.tsx
+++ src/stories/Button/Button.stories.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 // eslint-disable-next-line import/no-unresolved
 import { Story, Meta } from '@storybook/react/types-6-0'
+import { withDesign } from 'storybook-addon-designs'
 
 import { Button, ButtonProps } from './Button'
 
 export default {
   title: 'Example/Button',
   component: Button,
+  decorators: [withDesign],
   argTypes: {
     backgroundColor: { control: 'color' },
   },
@@ -19,6 +21,12 @@ export const Primary = Template.bind({})
 Primary.args = {
   primary: true,
   label: 'Button',
+}
+Primary.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/k1eUtEFmf177bihKWeLYXA/Storybook-Demo?node-id=1%3A1441',
+  },
 }
 
 export const Secondary = Template.bind({})
